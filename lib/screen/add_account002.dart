8b5acd1ed7d4f0f56f87fb5cd1e23ee5fa741595
hosecloud <4osecloud@gmail.com>
2022-04-09 03:05:35 +0900

    Refactor code in add_account002

--- lib/screen/add_account002.dart
+++ lib/screen/add_account002.dart
@@ -20,35 +20,88 @@ class _ScreenAddAccountState extends State<ScreenAddAccount> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView(
-        padding: const EdgeInsets.all(16.0),
-        children: <Widget>[
-          const Align(
-            alignment: Alignment.topLeft,
-            child: BackButton(
-              color: Colors.blue,
-            ),
-          ),
-          const Center(
-            child: Text(
-              "추가할 서비스가 무엇인가요?",
-              textScaleFactor: 3.0,
-              style: TextStyle(fontWeight: FontWeight.bold),
-            ),
-          ),
-          const SizedBox(height: 30.0),
-          TextField(
-            obscureText: false,
-            decoration: const InputDecoration(
-              icon: Icon(Icons.search),
-              border: OutlineInputBorder(),
-              labelText: 'Search',
-            ),
-            onChanged: _scanValue,
-          ),
-          const SizedBox(height: 30.0),
-          Text(_value),
-        ],
+      body: LayoutBuilder(
+        builder: (BuildContext context, BoxConstraints constraints) {
+          return Column(
+            children: <Widget>[
+              Container(
+                height: constraints.maxHeight * 0.1,
+                alignment: Alignment.centerLeft,
+                child: const BackButton(
+                  color: Colors.blue,
+                ),
+              ),
+              Container(
+                height: constraints.maxHeight * 0.3,
+                alignment: Alignment.center,
+                child: Column(
+                  children: <Widget>[
+                    Container(
+                      height: constraints.maxHeight * 0.15,
+                      alignment: Alignment.center,
+                      padding: const EdgeInsets.all(16.0),
+                      child: const Center(
+                        child: Text(
+                          "추가할 서비스가 무엇인가요?",
+                          textScaleFactor: 2.5,
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                    ),
+                    Container(
+                      height: constraints.maxHeight * 0.15,
+                      alignment: Alignment.center,
+                      padding: const EdgeInsets.all(16.0),
+                      child: TextField(
+                        obscureText: false,
+                        decoration: const InputDecoration(
+                          icon: Icon(Icons.search),
+                          border: OutlineInputBorder(),
+                          labelText: 'Search',
+                        ),
+                        onChanged: _scanValue,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                height: constraints.maxHeight * 0.6,
+                alignment: Alignment.center,
+                padding: const EdgeInsets.all(16.0),
+                child: ListView(
+                  restorationId: 'test_view',
+                  padding: const EdgeInsets.all(16.0),
+                  children: <Widget>[
+                    for(int index = 0; index < 10; index++)
+                      Card(
+                        child: InkWell(
+                          splashColor: Colors.blue.withAlpha(30),
+                          onTap: () {
+                          debugPrint('Card tapped.');
+                          },
+                          child: const SizedBox(
+                            width: 300,
+                            height: 100,
+                            child: Center(
+                              child: ListTile(
+                                leading: FlutterLogo(size: 100.0),
+                                title: Text(
+                                  'OTT service\'s name',
+                                  style: TextStyle(fontWeight: FontWeight.bold),
+                                  textScaleFactor: 2.0,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                  ],
+                ),
+              ),
+            ],
+          );
+        }
       ),
     );
   }
