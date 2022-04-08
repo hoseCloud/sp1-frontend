138cfb19a5c5a34c0fe2477603171ec76f41cea7
hosecloud <4osecloud@gmail.com>
2022-04-08 18:18:13 +0900

    Add scarch function in add_account002

--- lib/screen/add_account002.dart
+++ lib/screen/add_account002.dart
@@ -1,37 +1,53 @@
 import 'package:flutter/material.dart';
 
 // #002 ScreenAddAccount
-class ScreenAddAccount extends StatelessWidget {
+class ScreenAddAccount extends StatefulWidget {
   const ScreenAddAccount({Key? key}) : super(key: key);
+
+  @override
+  State<ScreenAddAccount> createState() => _ScreenAddAccountState();
+}
+
+class _ScreenAddAccountState extends State<ScreenAddAccount> {
+  String _value = '';
+
+  void _scanValue(String value) {
+    setState(() {
+      _value = value;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: ListView(
         padding: const EdgeInsets.all(16.0),
-        children: const <Widget>[
-          Align(
+        children: <Widget>[
+          const Align(
             alignment: Alignment.topLeft,
             child: BackButton(
               color: Colors.blue,
             ),
           ),
-          Center(
+          const Center(
             child: Text(
               "추가할 서비스가 무엇인가요?",
               textScaleFactor: 3.0,
               style: TextStyle(fontWeight: FontWeight.bold),
             ),
           ),
-          SizedBox(height: 30.0),
+          const SizedBox(height: 30.0),
           TextField(
             obscureText: false,
-            decoration: InputDecoration(
+            decoration: const InputDecoration(
               icon: Icon(Icons.search),
               border: OutlineInputBorder(),
               labelText: 'Search',
             ),
+            onChanged: _scanValue,
           ),
+          const SizedBox(height: 30.0),
+          Text(_value),
         ],
       ),
     );
