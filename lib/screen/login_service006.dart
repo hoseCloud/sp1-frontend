9d8f2362dedb68b6b29f1c7ce95fa881852f5642
hosecloud <4osecloud@gmail.com>
2022-04-14 01:37:00 +0900

    [#6] Remove unnecessary package

--- lib/screen/login_service006.dart
+++ lib/screen/login_service006.dart
@@ -1,9 +1,6 @@
-import 'dart:html';
-
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
-import 'dart:async';
 
 // #006 ScreenLoginService
 class ScreenLoginService extends StatefulWidget {
