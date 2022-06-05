4c1528ea5eea5f6d58db764a6ebca4f43fe0561c
hosecloud <4osecloud@gmail.com>
2022-06-06 02:12:24 +0900

    Remove test code

--- lib/screen/setting003.dart
+++ lib/screen/setting003.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutterapp/screen/login_member004.dart';
 import 'package:flutterapp/screen/register_member005.dart';
 import 'package:flutterapp/screen/login_service006.dart';
-import 'package:flutterapp/screen/test_stats.dart';
 
 // #003 ScreenSetting
 class ScreenSetting extends StatelessWidget {
@@ -12,109 +11,85 @@ class ScreenSetting extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: ListView(
-          padding: const EdgeInsets.all(16.0),
-          children: <Widget>[
-            Card(
-              child: InkWell(
-                splashColor: Colors.blue.withAlpha(30),
-                onTap: () {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => const ScreenLoginMember())
-                  );
-                },
-                child: const SizedBox(
-                  width: 300,
-                  height: 100,
-                  child: Center(
-                    child: ListTile(
-                      leading: Icon(Icons.login),
-                      title: Text(
-                        'Login to member',
-                        style: TextStyle(fontWeight: FontWeight.bold),
-                        textScaleFactor: 2.0,
-                      ),
+        padding: const EdgeInsets.all(16.0),
+        children: <Widget>[
+          Card(
+            child: InkWell(
+              splashColor: Colors.blue.withAlpha(30),
+              onTap: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const ScreenLoginMember()));
+              },
+              child: const SizedBox(
+                width: 300,
+                height: 100,
+                child: Center(
+                  child: ListTile(
+                    leading: Icon(Icons.login),
+                    title: Text(
+                      'Login to member',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                      textScaleFactor: 2.0,
                     ),
                   ),
                 ),
               ),
             ),
-            Card(
-              child: InkWell(
-                splashColor: Colors.blue.withAlpha(30),
-                onTap: () {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => const ScreenRegisterMember())
-                  );
-                },
-                child: const SizedBox(
-                  width: 300,
-                  height: 100,
-                  child: Center(
-                    child: ListTile(
-                      leading: Icon(Icons.app_registration),
-                      title: Text(
-                        'Register to member',
-                        style: TextStyle(fontWeight: FontWeight.bold),
-                        textScaleFactor: 2.0,
-                      ),
+          ),
+          Card(
+            child: InkWell(
+              splashColor: Colors.blue.withAlpha(30),
+              onTap: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const ScreenRegisterMember()));
+              },
+              child: const SizedBox(
+                width: 300,
+                height: 100,
+                child: Center(
+                  child: ListTile(
+                    leading: Icon(Icons.app_registration),
+                    title: Text(
+                      'Register to member',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                      textScaleFactor: 2.0,
                     ),
                   ),
                 ),
               ),
             ),
-            Card(
-              child: InkWell(
-                splashColor: Colors.blue.withAlpha(30),
-                onTap: () {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => const ScreenLoginService(serviceName: 'netflix'))
-                  );
-                },
-                child: const SizedBox(
-                  width: 300,
-                  height: 100,
-                  child: Center(
-                    child: ListTile(
-                      leading: Icon(Icons.login),
-                      title: Text(
-                        'Login to Netflix',
-                        style: TextStyle(fontWeight: FontWeight.bold),
-                        textScaleFactor: 2.0,
-                      ),
+          ),
+          Card(
+            child: InkWell(
+              splashColor: Colors.blue.withAlpha(30),
+              onTap: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) =>
+                            const ScreenLoginService(serviceName: 'netflix')));
+              },
+              child: const SizedBox(
+                width: 300,
+                height: 100,
+                child: Center(
+                  child: ListTile(
+                    leading: Icon(Icons.login),
+                    title: Text(
+                      'Login to Netflix',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                      textScaleFactor: 2.0,
                     ),
                   ),
                 ),
               ),
             ),
-            Card(
-              child: InkWell(
-                splashColor: Colors.blue.withAlpha(30),
-                onTap: () {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => const StatsTest())
-                  );
-                },
-                child: const SizedBox(
-                  width: 300,
-                  height: 100,
-                  child: Center(
-                    child: ListTile(
-                      leading: Icon(Icons.login),
-                      title: Text(
-                        'Test stats',
-                        style: TextStyle(fontWeight: FontWeight.bold),
-                        textScaleFactor: 2.0,
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-            ),
-          ],
+          ),
+        ],
       ),
     );
   }
